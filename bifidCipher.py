65bbb3ffff17931bdd1a9f6f45858376f765e774
Diego Palacios <48029515+diegoopr@users.noreply.github.com>
2022-09-19 18:13:32 -0500

    Bifid Cipher
    This file works up to the part where it finds the coordinates of the message to cipher and reads them as a single string.

---
+++ bifidCipher.py
@@ -0,0 +1,113 @@
+"""
+Palacios Rodriguez Diego Octavio
+Bifid Cypher
+"""
+import re
+
+
+def checkRepeat(secretKey):
+    nonRepeated = ""
+    
+    for char in secretKey:
+        if nonRepeated == "":
+            nonRepeated += char
+            continue
+            
+        if char in nonRepeated:
+            continue
+        else:
+            nonRepeated += char
+    print(nonRepeated)
+    return(nonRepeated)
+        
+
+def fillMatrix(nonRepeated):
+    nonRepeated.replace("j", "i")
+    alphabet = "abcdefghiklmnopqrstuvwxyz" #no J
+    row = 0
+    col = 0
+    matrix = [[0 for i in range(5)] for j in range(5)]
+
+    for letter in nonRepeated:
+        if col == 5:
+            row += 1
+            col = 0
+        
+        matrix[row][col] = letter
+        col += 1
+
+    for letter in alphabet:
+        if col == 5:
+            row += 1
+            col = 0
+
+        if letter in nonRepeated:
+            continue
+        else:
+            matrix[row][col] = letter
+            col += 1
+
+    return(matrix)
+    """
+    for i in range(5):
+        print("=============================")
+        for j in range(5):
+            print( "| ", matrix[i][j],  " |")
+    print("=============================")
+    """
+
+
+def cypher(message, matrix):
+    findCoords(message, matrix)
+
+
+def findCoords(message, matrix):
+    print("work in progress")
+    coordsRow = list()
+    coordsCol = list()
+    for letters in message:
+
+        for i in range(5):
+            for j in range (5):
+                if matrix[i][j] == letters:
+                    coordsRow.append(i)
+                    coordsCol.append(j)
+                    continue
+    
+    print(coordsRow)
+    print(coordsCol)
+    #length = len(coordsRow) #given that a coordinate is conformed by two numbers, len(coordsRow) must be == to len(coordsCol)
+
+    finalCoords = list()
+    for letters in coordsRow:
+        finalCoords.append(letters)
+    
+    for letters in coordsCol:
+        finalCoords.append(letters)
+
+    print(finalCoords)
+    return(finalCoords)
+
+
+while(True):
+    secretKey = input("Please type your secret key.\nYour key should only contain letters in the English alphabet: \n>>")
+    if (re.search("Ññ[0-9]", secretKey)):
+        print("Please type a message that contains ONLY characters in the English alphabet.")
+    else: 
+        matrix = fillMatrix(checkRepeat(secretKey.lower()))
+        print(matrix)
+        break
+
+
+
+while(True):
+    message = input("\nPlease type the message you wish to cypher.\nYour message should only contain letters in the English alphabet: \n>>")
+    if (re.search("Ññ[0-9]", message)):
+        print("Please type a message that contains ONLY characters in the English alphabet.")
+    else:
+        cypher(message.lower(), matrix)
+        break
+
+
+
+
